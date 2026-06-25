70f3b96f03ae35bf784f81cdeec03559bb0e8aac
Abdullahi Hashim Bala <ahashimballer@gmail.com>
2026-06-25 11:53:18 +0100

    fix server and multer

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,7 +1,8 @@
 import multer from "multer";
+import os from "os";
 
 const storage = multer.diskStorage({
-    destination: "uploads",
+    destination: process.env.NODE_ENV === 'production' ? os.tmpdir() : "uploads",
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}_${file.originalname}`);
     }
--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
 app.use("/images", express.static("uploads"));
-
 
 app.use(async(req, res, next) => {
     try {
