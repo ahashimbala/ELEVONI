d7ba58afe719bbd23d9bfe0e91194e1f7e2a1a13
Abdullahi Hashim Bala <ahashimballer@gmail.com>
2026-06-25 10:49:39 +0100

    fixed server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config';
 
-import { connectDB } from "./config/db.js";
+import connectDB from "./config/db.js";
 
 import fishRouter from "./routes/fishRoute.js";
 import userRouter from "./routes/userRoute.js";
