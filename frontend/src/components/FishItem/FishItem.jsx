17cb0e1bf3ad23a63a695d7b48253203a73efd6f
Abdullahi Hashim Bala <ahashimballer@gmail.com>
2026-06-27 12:56:02 +0100

    go to cart implementation

--- frontend/src/components/FishItem/FishItem.jsx
+++ frontend/src/components/FishItem/FishItem.jsx
@@ -1,50 +1,58 @@
 import React, { useContext } from "react";
 import "./FishItem.css";
-import { assets } from "../../assets/assets";
 import { StoreContext } from "../../context/StoreContext";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const FishItem = ({ id, name, image, price, description, category }) => {
+const FishItem = ({ id, name, image, price, description }) => {
   const { cartItems, addtoCart, removeFromCart, url } =
     useContext(StoreContext);
   const navigate = useNavigate();
 
   return (
     <div className="fish-item">
-      <div className="fish-item-img-container">
-        <img
-          className="fish-item-image"
-          src={
-            image && image.startsWith("http") ? image : `${url}/images/${image}`
-          }
-          alt={name}
-        />
-        <div className="fish-item-info">
-          <div className="fish-item-name-rating">
-            <p>{name}</p>
-          </div>
-          <p className="fish-item-desc">{description}</p>
-          <p className="fish-item-price">
-            ₦{price}
-            <span id="qty"> per kg</span>
-          </p>
+      <img
+        className="fish-item-image"
+        src={
+          image && image.startsWith("http") ? image : `${url}/images/${image}`
+        }
+        alt={name}
+      />
+
+      <div className="fish-item-info">
+        <div className="fish-item-name-rating">
+          <p>{name}</p>
         </div>
-        <div className="buttons">
+        <p className="fish-item-desc">{description}</p>
+        <p className="fish-item-price">
+          ₦{price.toLocaleString()}
+          <span id="qty"> per kg</span>
+        </p>
+
+        <div className="buttons-container">
           {!cartItems?.[id] ? (
-            <button className="cart-btn" onClick={() => addtoCart(id)}>
+            <button
+              className="cart-btn init-add-btn"
+              onClick={() => addtoCart(id)}
+            >
               Add to Cart
             </button>
           ) : (
-            <div className="fish-item-counter">
-              <button onClick={() => removeFromCart(id)}>
-                Remove from Cart
-              </button>
-              <p>{cartItems[id]}</p>
-              <button className="cart-btn" onClick={() => addtoCart(id)}>
-                Add to Cart
+            <div className="active-cart-row">
+              <div className="fish-item-counter">
+                <button onClick={() => removeFromCart(id)}>-</button>
+                <span>{cartItems[id]}</span>
+                <button onClick={() => addtoCart(id)}>+</button>
+              </div>
+
+              <button
+                className="checkout-badge-btn"
+                onClick={() => navigate("/cart")}
+              >
+                Go to Cart →
               </button>
             </div>
           )}
+
           <button
             className="details-btn"
             onClick={() => navigate(`/product/${id}`)}
