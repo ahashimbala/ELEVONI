fef3ecfae0ef85fa957e1cb20addc33098a8ac0d
Abdullahi Hashim Bala <ahashimballer@gmail.com>
2026-07-03 09:35:40 +0100

    server logic updated

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,8 @@ import cartRouter from "./routes/cartRoute.js";
 import reviewRouter from "./routes/reviewRoute.js";
 import orderRouter from "./routes/orderRoute.js";
 
+import { FishItem } from "./models/fishModel.js";
+
 const app = express();
 
 const allowedOrigins = [
@@ -37,6 +39,38 @@ app.use(cors({
 app.use(express.json());
 
 connectDB();
+
+app.get("/sitemap.xml", async(req, res) => {
+    try {
+        const items = await FishItem.find({}, "_id updatedAt");
+
+        let xml = `<?xml version="1.0" encoding="UTF-8"?>\n`;
+        xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+
+        xml += `  <url>\n    <loc>https://elevonifarms.vercel.app/</loc>\n    <priority>1.00</priority>\n    <changefreq>daily</changefreq>\n  </url>\n`;
+
+        items.forEach((item) => {
+            const lastModDate = item.updatedAt ?
+                new Date(item.updatedAt).toISOString().split("T")[0] :
+                new Date().toISOString().split("T")[0];
+
+            xml += `  <url>\n`;
+            xml += `    <loc>https://elevonifarms.vercel.app/product/${item._id}</loc>\n`;
+            xml += `    <lastmod>${lastModDate}</lastmod>\n`;
+            xml += `    <changefreq>weekly</changefreq>\n`;
+            xml += `    <priority>0.80</priority>\n`;
+            xml += `  </url>\n`;
+        });
+
+        xml += `</urlset>`;
+
+        res.header("Content-Type", "application/xml");
+        return res.status(200).send(xml);
+    } catch (error) {
+        res.header("Content-Type", "application/xml");
+        return res.status(500).send(`<?xml version="1.0" encoding="UTF-8"?><error>Error generating sitemap</error>`);
+    }
+});
 
 app.use("/images", express.static("uploads"));
 
